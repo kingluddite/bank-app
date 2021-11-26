f8418693a77dff1cf42737c9b1b12da38a213359
philip howley <howley.phil@gmail.com>
2021-11-26 17:26:05 -0500

    Remove api

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(
-    'mongodb+srv://philadmin:qGiOugTkFJ5ROG6d@devconnector.a2gjt.mongodb.net/bankApp',
+  '',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
