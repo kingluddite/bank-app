6b1384cb87034b29432a1f327f876c1cbff624f0
philip howley <howley.phil@gmail.com>
2021-11-27 18:18:22 -0500

    Auth working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import UserContext from './context/userContext';
 import Header from './components/layout/Header';
 import Home from './components/pages/Home';
 import Register from './auth/Register';
 import Login from './auth/Login';
+import './App.css';
 
 function App() {
   const [userData, setUserData] = useState({
     token: undefined,
     user: undefined,
   });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+      const tokenResponse = await axios.post(
+        'http://localhost:4000/users/tokenIsValid',
+        null,
+        { headers: { 'x-auth-token': token } }
+      );
+      if (tokenResponse.data) {
+        const userRes = await axios.get('http://localhost:4000/users/', {
+          headers: { 'x-auth-token': token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
 
   return (
     <BrowserRouter>
