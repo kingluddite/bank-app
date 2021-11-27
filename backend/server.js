8eae685fd7f94b56d1e2ca0d2bd0c61f8d827c18
philip howley <howley.phil@gmail.com>
2021-11-26 22:49:56 -0500

    Update server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// set up express
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
+// database
 mongoose.connect(
-  '',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,3 +24,11 @@ mongoose.connect(
 );
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up routes
+// test a simple get route to the home page
+// app.get('/', (req, res) => {
+//   res.send('home page works');
+// });
+app.use('/users', require('./routes/users'));
+app.use('/todos', require('./routes/todos'));
