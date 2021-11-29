f29fb24e301628690cec2aae7438544ffdf254b5
philip howley <howley.phil@gmail.com>
2021-11-29 17:07:14 -0500

    Add basic layout components and register and some auth

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bank App
 ## Resources
 * https://levelup.gitconnected.com/authentication-using-jwt-in-mern-1cc5c8ccd03c
+
+## Frontend
+### Recouces
+* https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from './context/userContext';
+import Header from './components/layout/Header';
+import Home from './components/pages/Home';
+
 function App() {
-  return <div>Yo Ivana!</div>;
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  return (
+    <BrowserRouter>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Header />
+        <Routes>
+          <Route exact path="/" component={Home} />
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
