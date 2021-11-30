a054355db81620c141e7d1baa05e98347f54e60c
philip howley <howley.phil@gmail.com>
2021-11-29 21:53:42 -0500

    Modularize db connection and setup local db if needed

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+// you need this to send data from one domain to another domain
+// if you don't have cors you will get this error (https://i.imgur.com/sNUz4my.png)
 const cors = require('cors');
-require('dotenv').config();
 
 // set up express
 const app = express();
+// you need this next line to use req.body else you get "undefined"
 app.use(express.json());
+// make sure to use the cors middleware (next line!)
 app.use(cors());
+
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 4000;
 
-// database
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log('MongoDB connection established');
-  }
-);
-
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+// database (i made code modular and put db connection in it's own folder)
+// mongoose.connect(
+//   process.env.MONGODB_URI,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) throw err;
+//     console.log('MongoDB connection established');
+//   }
+// );
 
 // set up routes
 // test a simple get route to the home page
@@ -32,3 +36,9 @@ app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 // });
 app.use('/users', require('./routes/users'));
 app.use('/todos', require('./routes/todos'));
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`The server has started on port: ${PORT}`);
+  });
+});
