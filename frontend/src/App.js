afd06da29a6874ae2c50f8f1b6244dbf205dae57
philip howley <howley.phil@gmail.com>
2021-12-02 16:41:38 -0500

    Fix react router error and get home and register routes working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/">
-            Hi
-          </Route>
-          <Route exact path="/">
-            <Register />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
       </Router>
     </div>
   );
