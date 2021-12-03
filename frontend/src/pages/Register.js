d864bdf9a409fd4f9ffddd714df7cf3ac5ca01da
philip howley <howley.phil@gmail.com>
2021-12-02 22:36:23 -0500

    Style error message with styled components

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledError = styled.span`
+  color: red;
+`;
+
+const handleErrors = async (response) => {
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw Error(message);
+  }
+  return response.json();
+};
 
 function Register() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const register = (e) => {
     e.preventDefault();
@@ -15,13 +29,18 @@ function Register() {
         username,
         password,
       }),
-    });
-    // console.log('register works');
+    })
+      .then(handleErrors)
+      .then(() => {})
+      .catch((error) => {
+        setError(error.message);
+      });
   };
 
   return (
     <>
       <h1>Register</h1>
+      {!!error && <StyledError>{error}</StyledError>}
       <form onSubmit={register}>
         <input
           onChange={(e) => setUsername(e.target.value)}
