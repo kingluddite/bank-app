ded64acea1b77eefbe3acfee78524407574c98f7
philip howley <howley.phil@gmail.com>
2021-12-03 18:25:00 -0500

    tasks state toggles but not persistantly

--- frontend/src/components/Todos.js
+++ frontend/src/components/Todos.js
@@ -7,18 +7,47 @@ function Todos() {
       text: 'yo homee',
     },
   ]);
+  const [todoText, setTodoText] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    // prevent empty tasks
+    if (!todoText) return;
+    // create a new todo
+    const newTodo = { checked: false, text: todoText };
+    setTodos([...todos, newTodo]);
+    setTodoText('');
+  };
+
+  const toggleTodo = (index) => {
+    // get all todos
+    const newTodoList = [...todos];
+    // toggle opposite checked value
+    newTodoList[index].checked = !newTodoList[index];
+    setTodos(newTodoList);
+  };
 
   return (
     <div>
       {todos.map((todo, index) => (
         <div key={index}>
-          <input id="chkbx-task" type="checkbox" />
+          <input
+            id="chkbx-task"
+            onChange={() => toggleTodo(index)}
+            type="checkbox"
+          />
           <label htmlFor="chkbx-task">{todo.text}</label>
         </div>
       ))}
       <br />
-      <input type="text" />
-      <button>Add</button>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
