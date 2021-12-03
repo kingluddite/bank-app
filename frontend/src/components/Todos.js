412d8441ee31a131a07ddb541253b6345a702a83
philip howley <howley.phil@gmail.com>
2021-12-03 17:06:05 -0500

    use index instead of id for map

--- frontend/src/components/Todos.js
+++ frontend/src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function Todos() {
-  const [todos, setTods] = useState([
+  const [todos, setTodos] = useState([
     {
       id: 1,
       text: 'yo homee',
@@ -10,8 +10,8 @@ function Todos() {
 
   return (
     <div>
-      {todos.map((todo) => (
-        <div key={todo.id}>
+      {todos.map((todo, index) => (
+        <div key={index}>
           <input id="chkbx-task" type="checkbox" />
           <label htmlFor="chkbx-task">{todo.text}</label>
         </div>
