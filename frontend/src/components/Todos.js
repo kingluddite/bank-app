9b258d7de6ff7922874ac1294b95460affa592d8
philip howley <howley.phil@gmail.com>
2021-12-03 17:05:10 -0500

    make todo work with tmp data

--- frontend/src/components/Todos.js
+++ frontend/src/components/Todos.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
 
 function Todos() {
+  const [todos, setTods] = useState([
+    {
+      id: 1,
+      text: 'yo homee',
+    },
+  ]);
+
   return (
     <div>
-      <p>Todos</p>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <input id="chkbx-task" type="checkbox" />
+          <label htmlFor="chkbx-task">{todo.text}</label>
+        </div>
+      ))}
+      <br />
+      <input type="text" />
+      <button>Add</button>
     </div>
   );
 }
