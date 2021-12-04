479dc05a7ba9b5efed71f5468c260b5c795773c8
philip howley <howley.phil@gmail.com>
2021-12-03 21:26:16 -0500

    Add working persist for todos

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,25 @@ const PORT = process.env.PORT || 4000;
 
 // create an alternative user
 // just to play around with alternate way to authenticate
+// Models
 const altUserSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
+
 const AltUser = mongoose.model('AltUser', altUserSchema);
+
+const altTodosSchema = new mongoose.Schema({
+  userId: String,
+  todos: [
+    {
+      check: Boolean,
+      text: String,
+    },
+  ],
+});
+
+const AltTodos = mongoose.model('AltTodos', altTodosSchema);
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
@@ -63,10 +77,41 @@ app.post('/login', async (req, res) => {
   });
 });
 
-app.post('/todos', (req, res) => {
-  // let's test "hitting" this endpoint
-  // use a tool like Postman or Insomnia to test post API calls
-  res.send('todos endpoint hit');
+app.post('/todos', async (req, res) => {
+  // First - test hitting endpoing
+  // (use a tool like Postman or Insomnia to test post API calls)
+  // res.send('todos endpoint hit');
+  // Second - get the username and password with some JavaScript ninja skills :)
+  // 1) grab what in authorization from the request's headers
+  const { authorization } = req.headers;
+  // console.log(authorization); // (will output this) Basic pip:123456
+  // 2) split the items by the space and store the second part in the variable token
+  const [, token] = authorization.split(' ');
+  // 3) split into two items "username" and "password" based on ":"
+  const [username, password] = token.split(':');
+  // grab the todos sent over from the client
+  const todosItems = req.body;
+  const altUser = await AltUser.findOne({ username }).exec();
+  if (!altUser || altUser.password !== password) {
+    res.status(401);
+    res.json({
+      message: 'invalid login',
+    });
+    return;
+  }
+  const todos = await AltTodos.findOne({ userId: altUser._id }).exec();
+  if (!todos) {
+    // no todos for this user so let's create some todos
+    await AltTodos.create({
+      userId: altUser._id,
+      todos: todosItems,
+    });
+  } else {
+    // todos already exists
+    todos.todos = todosItems;
+    await todos.save();
+  }
+  res.json(todos);
 });
 
 // app.use('/users', require('./routes/users'));
